539dee8dfa9e438307f319ec4336ba8b7b71511a
OlesiaPastushenko <103423216+OlesyaPastushenko@users.noreply.github.com>
2022-12-23 11:08:14 +0100

    Added Mobile menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import MobileMenu from './Mobile_menu';
 
 function App() {
   console.log("test2")
   console.log("hello")
   return (
     <div className="App">
-      <h1>Hello from the outside</h1>hello
+      <h1>Hello from the outside</h1>
+      <MobileMenu/>
     </div>
   );
 }

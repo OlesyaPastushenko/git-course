6b7971c7368f0e3e4b01673f5f484fe0af03259d
OlesiaPastushenko <103423216+OlesyaPastushenko@users.noreply.github.com>
2022-12-23 10:30:51 +0100

    change in console.log

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   console.log("test2")
-  console.log("test3")
+  console.log("hello")
   return (
     <div className="App">
       <h1>Hello from the outside</h1>hello

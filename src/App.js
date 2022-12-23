40fdae5374ca308d547d6999e0079c447d1bf14e
OlesiaPastushenko <103423216+OlesyaPastushenko@users.noreply.github.com>
2022-12-23 15:11:48 +0100

    without logs

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import MobileMenu from './Mobile_menu';
 
 function App() {
-  console.log("test2")
-  console.log("hello")
   return (
     <div className="App">
       <h1>Hello from the outside</h1>
